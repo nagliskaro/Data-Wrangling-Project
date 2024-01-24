c28f5fe3d10872f82551518d0426c6ac6f17d0d2
Bartolomeo Attolico <bartolomeo.attolico@gmail.com>
2024-01-24 15:14:46 +0100

    set up git repo

---
+++ project_dw.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def read_process_data():
+    pass
+
+
+def analyze_data():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
