6358039d0ef8439bd0d963843f95f78a60512390
Bartolomeo Attolico <bartolomeo.attolico@gmail.com>
2024-01-25 15:04:32 +0100

    milan and sassari data set

--- project_dw.py
+++ project_dw.py
@@ -3,31 +3,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# RQ: How does particle pollution from fine particulates (PM2.5) impact premature deaths in the European Union in the timespan from 2005 to 2020? 
+
+# (sub questions) What is the impact of other particles like N02 and O3 compared to PM 2.5 ? What is the trend over the years?
+
+
 
 def read_process_data(data_dir):
     # reading and cleaning PM 2.5 CSV
     dir_pm25 = "PM2.5-impact_premature_deaths.csv"
     data_pm25 = pd.read_csv(data_dir+dir_pm25)
-    data_pm25 = data_pm25.drop(columns=["City Boundary Specification (LAU/grid)", 'City', 'City Code', 'Health Risk Scenario'])
-    print(data_pm25.head())
+    data_pm25 = data_pm25.drop(columns=["City Boundary Specification (LAU/grid)", 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
+    # print(data_pm25.head())
 
     # reading and cleaning O3 CSV
     dir_O3 = "O3-impact_premature_deaths.csv"
     data_O3 = pd.read_csv(data_dir+dir_O3)
-    data_O3 = data_O3.drop(columns=["City Boundary Specification (LAU/grid)", 'City', 'City Code', 'Health Risk Scenario'])
-    print(data_O3.head())
+    data_O3 = data_O3.drop(columns=["City Boundary Specification (LAU/grid)", 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
+    # print(data_O3.head())
 
     # reading and cleaning N02 CSV
     dir_N02 = "N02-impact_premature_deaths.csv"
     data_N02 = pd.read_csv(data_dir+dir_N02)
-    data_N02 = data_N02.drop(columns=["City Boundary Specification (LAU/grid)", 'City', 'City Code', 'Health Risk Scenario'])
-    print(data_N02.head())
+    data_N02 = data_N02.drop(columns=["City Boundary Specification (LAU/grid)", 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
+    # print(data_N02.head())
     
     # combining the different polluants in a df
     polluants_df = pd.concat([data_pm25, data_O3, data_N02], ignore_index=True)
     polluants_df = polluants_df.sort_values(["Year", "Air Pollutant", "Country Or Territory"])
-    polluants_df.to_csv("combines_polluants.csv", encoding='utf-8', index=False)
-    print(polluants_df.head())
+    polluants_df.to_csv("cleaned_data/combines_polluants.csv", encoding='utf-8', index=False)
+    # print(polluants_df.head())
+    
+    # read in Milan data
+    dir_milan = "milan.csv"
+    df_milan = pd.read_csv(data_dir+dir_milan)
+    df_milan = df_milan.drop(columns=["City Boundary Specification (LAU/grid)", 'Country Or Territory', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
+    print(df_milan.head())
+    df_milan.to_csv("cleaned_data/milan.csv", encoding='utf-8', index=False)
+
+    # read in Sassari data
+    dir_sassar = "sassari.csv"
+    df_sassari = pd.read_csv(data_dir+dir_sassar)
+    df_sassari = df_sassari.drop(columns=["City Boundary Specification (LAU/grid)", 'Country Or Territory', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
+    print(df_sassari.head())
+    df_sassari.to_csv("cleaned_data/sassari.csv", encoding='utf-8', index=False)
     
     # Reading in CSV for total deaths attributed to air quality
     total_airqual_deaths = "total_deaths_attributed_to_air_quality.csv"
@@ -38,17 +57,20 @@ def read_process_data(data_dir):
                 'Dim3 type', 'Dim3', 'Dim3ValueCode', 'DataSourceDimValueCode', 'DataSource', 'FactValueNumericPrefix',
                 'FactValueUoM', 'FactValueNumericLowPrefix', 'FactValueNumericLow', 'FactValueNumericHighPrefix',
                 'FactValueNumericHigh', 'FactValueTranslationID', 'FactComments', "DateModified", "Language", "Period type"], axis=1)
-    df_airqual_deaths.to_csv("air_quality_attributed_deaths.csv", encoding='utf-8', index=False)
-
+    df_airqual_deaths.to_csv("cleaned_data/air_quality_attributed_deaths.csv", encoding='utf-8', index=False)
 
 
 def analyze_data():
-    pass
+    df_polution = pd.read_csv("cleaned_data/combines_polluants.csv")
+    df_milan = pd.read_csv("cleaned_data/milan.csv")
+    df_sassari = pd.read_csv("cleaned_data/sassari.csv")
 
 
 def main():
-    # data_dir = "data_sets/"
-    # read_process_data(data_dir)
+    
+    data_dir = "data_sets/"
+    read_process_data(data_dir)
+    
     analyze_data()
 
 
