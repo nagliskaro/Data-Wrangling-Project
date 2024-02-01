6a72593d62b6b77b5cc504f0d9f4361002401ba7
Bartolomeo Attolico <bartolomeo.attolico@gmail.com>
2024-02-01 20:05:56 +0100

    processing of europe cities data

--- project_dw.py
+++ project_dw.py
@@ -9,49 +9,41 @@ from sklearn.linear_model import LinearRegression
 # (sub questions) What is the impact of other particles like N02 and O3 compared to PM 2.5 ? What is the trend over the years?
 
 
-
 def read_process_data(data_dir):
     # reading and cleaning PM 2.5 CSV
     dir_pm25 = "PM2.5-impact_premature_deaths.csv"
     data_pm25 = pd.read_csv(data_dir+dir_pm25)
     data_pm25 = data_pm25.drop(columns=["City Boundary Specification (LAU/grid)", 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
-    # print(data_pm25.head())
 
     # reading and cleaning O3 CSV
     dir_O3 = "O3-impact_premature_deaths.csv"
     data_O3 = pd.read_csv(data_dir+dir_O3)
     data_O3 = data_O3.drop(columns=["City Boundary Specification (LAU/grid)", 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
-    # print(data_O3.head())
 
     # reading and cleaning N02 CSV
     dir_N02 = "N02-impact_premature_deaths.csv"
     data_N02 = pd.read_csv(data_dir+dir_N02)
     data_N02 = data_N02.drop(columns=["City Boundary Specification (LAU/grid)", 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
-    # print(data_N02.head())
     
     # combining the different polluants in a df
     polluants_df = pd.concat([data_pm25, data_O3, data_N02], ignore_index=True)
     polluants_df = polluants_df.sort_values(["Year", "Air Pollutant", "Country Or Territory"])
     polluants_df.to_csv("cleaned_data/combines_polluants.csv", encoding='utf-8', index=False)
-    # print(polluants_df.head())
     
     # read in Milan data
     dir_milan = "milan.csv"
     df_milan = pd.read_csv(data_dir+dir_milan)
     df_milan = df_milan.drop(columns=["City Boundary Specification (LAU/grid)", 'Country Or Territory', 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
-    #print(df_milan.head())
     df_milan.to_csv("cleaned_data/milan.csv", encoding='utf-8', index=False)
 
     # read in Sassari data
     dir_sassar = "sassari.csv"
     df_sassari = pd.read_csv(data_dir+dir_sassar)
     df_sassari = df_sassari.drop(columns=["City Boundary Specification (LAU/grid)", 'Country Or Territory', 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
-    #print(df_sassari.head())
     df_sassari.to_csv("cleaned_data/sassari.csv", encoding='utf-8', index=False)
     
     # Reading in CSV for total deaths attributed to air quality
     total_airqual_deaths = "total_deaths_attributed_to_air_quality.csv"
-    
     df_airqual_deaths = pd.read_csv(data_dir+total_airqual_deaths)
     df_airqual_deaths = df_airqual_deaths.drop(['IndicatorCode', 'Indicator', 'ValueType', 'ParentLocationCode', 'ParentLocation', 'Location type', 
                 'SpatialDimValueCode', 'IsLatestYear', 'Dim1 type', 'Dim1ValueCode', 'Dim2 type', 'Dim2', 'Dim2ValueCode',
@@ -59,6 +51,14 @@ def read_process_data(data_dir):
                 'FactValueUoM', 'FactValueNumericLowPrefix', 'FactValueNumericLow', 'FactValueNumericHighPrefix',
                 'FactValueNumericHigh', 'FactValueTranslationID', 'FactComments', "DateModified", "Language", "Period type"], axis=1)
     df_airqual_deaths.to_csv("cleaned_data/air_quality_attributed_deaths.csv", encoding='utf-8', index=False)
+    
+    # cleaning european main cities data sets
+    dir_list = ["milan", "berlin", "paris", "warsaw", "amsterdam", "dublin", "ljubljana", "stockholm", "zagreb", "helsinki"]
+    for i in dir_list:
+        df_city = pd.read_csv(data_dir+"europe_cities_pm25/"+i+".csv")
+        df_city = df_city.drop(columns=["City Boundary Specification (LAU/grid)", 'Country Or Territory', 'City Code', 'Health Risk Scenario', "Air Pollution Average [ug/m3]", 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
+        df_city.to_csv("cleaned_data/europe_cities_pm25/"+i+".csv", encoding='utf-8', index=False)
+        
 
 
 def analyze_data():
@@ -120,8 +120,7 @@ def analyze_data():
         plt.xlabel('Year')
         plt.ylabel('Values')
         plt.legend()
-        plt.tight_layout()
-        
+        plt.tight_layout()        
         
     # linear regression for all pollutants:
     plt.figure(figsize=(12, 4 * n_pollutants))
@@ -145,9 +144,12 @@ def analyze_data():
         plt.ylabel('Premature Deaths')
         plt.legend()
         plt.tight_layout()
+    plt.show()
+    
+    # plotting main european cities
         
     # Plotting Milan info: Air Pollutants and Premature Deaths
-     
+    
 def main():
     data_dir = "data_sets/"
     read_process_data(data_dir)
