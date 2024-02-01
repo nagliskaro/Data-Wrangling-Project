ea19850068dc1b89ebbbb8a44b8b98a87b67528c
naglisLT <naglis.karoblis@yahoo.com>
2024-02-01 18:23:32 +0100

    first commit

--- project_dw.py
+++ project_dw.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
+
 # RQ: What is the impact of these polluants (N02, O3, PM 2.5) on prematures deaths in Sassari (sardegna, Italya) and Milan (Lombardy, Italy)? And how do they compare ?
 # (sub questions) What is the impact of other particles like N02 and O3 compared to PM 2.5 ? What is the trend over the years?
+
+
 
 def read_process_data(data_dir):
     # reading and cleaning PM 2.5 CSV
@@ -36,14 +39,14 @@ def read_process_data(data_dir):
     dir_milan = "milan.csv"
     df_milan = pd.read_csv(data_dir+dir_milan)
     df_milan = df_milan.drop(columns=["City Boundary Specification (LAU/grid)", 'Country Or Territory', 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
-    print(df_milan.head())
+    #print(df_milan.head())
     df_milan.to_csv("cleaned_data/milan.csv", encoding='utf-8', index=False)
 
     # read in Sassari data
     dir_sassar = "sassari.csv"
     df_sassari = pd.read_csv(data_dir+dir_sassar)
     df_sassari = df_sassari.drop(columns=["City Boundary Specification (LAU/grid)", 'Country Or Territory', 'City', 'City Code', 'Health Risk Scenario', 'Premature Deaths - lower CI', 'Premature Deaths - upper CI', 'Years Of Life Lost', 'Years Of Life Lost - lower CI', 'Years Of Life Lost - upper CI'])
-    print(df_sassari.head())
+    #print(df_sassari.head())
     df_sassari.to_csv("cleaned_data/sassari.csv", encoding='utf-8', index=False)
     
     # Reading in CSV for total deaths attributed to air quality
@@ -61,47 +64,73 @@ def read_process_data(data_dir):
 def analyze_data():
     df_polution = pd.read_csv("cleaned_data/combines_polluants.csv").dropna()
     df_milan = pd.read_csv("cleaned_data/milan.csv").dropna()
+    #print(df_milan)
     df_sassari = pd.read_csv("cleaned_data/sassari.csv").dropna()
     
     sassari_group_mean = df_sassari.groupby("Air Pollutant").mean()
-    print(sassari_group_mean)
-
-    milan_group_mean = df_milan.groupby("Air Pollutant").mean()
-    print(milan_group_mean)
+    #print(sassari_group_mean)
     
-    sassari_group_polluant_year = df_sassari.groupby(["Air Pollutant", 'Year'])[["Air Pollution Average [ug/m3]", "Premature Deaths"]].mean()
-    print(sassari_group_polluant_year)
+    milan_group_mean = df_milan.groupby("Air Pollutant").mean()
+    #print(milan_group_mean)
+    
+    sassari_group_polluant_year = df_sassari.groupby(["Air Pollutant", 'Year'])[["Air Pollution Population Weighted Average [ug/m3]", "Premature Deaths"]].mean()
+    #print(sassari_group_polluant_year)
 
-    # plotting
+    # plotting Sassari Air Pollutants and Number of Premature Deaths
     pollutants = df_sassari['Air Pollutant'].unique()
     n_pollutants = len(pollutants)
+    
+    #milan
+    milan_group_polluant_year = df_milan.groupby(["Air Pollutant", 'Year'])[["Air Pollution Population Weighted Average [ug/m3]", "Premature Deaths"]].mean()
+    pollutants_milan = df_milan['Air Pollutant'].unique()
+    n_pollutants_milan = len(pollutants_milan)
 
+
+    plt.figure(figsize=(12, 4 * n_pollutants))
+    
+    for i, pollutant in enumerate(pollutants, 1):
+        plt.subplot(n_pollutants_milan, 1, i)
+        subset_milan = milan_group_polluant_year.loc[pollutant]
+        print(subset_milan)
+
+        plt.plot(subset_milan.index, subset_milan['Air Pollution Population Weighted Average [ug/m3]'], label='Air Pollution')
+        plt.plot(subset_milan.index, subset_milan['Premature Deaths'], label='Premature Deaths', linestyle='--')
+
+        plt.title(f'Trends for {pollutant} Milan')
+        plt.xlabel('Year')
+        plt.ylabel('Values')
+        plt.legend()
+        plt.tight_layout()
+    
     plt.figure(figsize=(12, 4 * n_pollutants))
 
     for i, pollutant in enumerate(pollutants, 1):
         plt.subplot(n_pollutants, 1, i)
         subset = sassari_group_polluant_year.loc[pollutant]
+        print(subset)
 
-        plt.plot(subset.index, subset['Air Pollution Average [ug/m3]'], label='Air Pollution')
+        plt.plot(subset.index, subset['Air Pollution Population Weighted Average [ug/m3]'], label='Air Pollution')
         plt.plot(subset.index, subset['Premature Deaths'], label='Premature Deaths', linestyle='--')
         
         # added a correlation coefficient
-        correlation_coefficient = subset['Air Pollution Average [ug/m3]'].corr(subset['Premature Deaths'])
+        correlation_coefficient = subset['Air Pollution Population Weighted Average [ug/m3]'].corr(subset['Premature Deaths'])
         plt.text(0.5, 0.9, f'Correlation: {correlation_coefficient:.2f}', transform=plt.gca().transAxes, fontsize=12, ha='center')
 
-        plt.title(f'Trends for {pollutant}')
+        plt.title(f'Trends for {pollutant} Sassari')
         plt.xlabel('Year')
         plt.ylabel('Values')
         plt.legend()
         plt.tight_layout()
         
+        
     # linear regression for all pollutants:
-    plt.figure(figsize=(12, 4 * len(pollutants)))
+    plt.figure(figsize=(12, 4 * n_pollutants))
+    
     for i, pollutant in enumerate(pollutants, 1):
         plt.subplot(len(pollutants), 1, i)
         
-        pollutant_data_sassari = df_sassari[df_sassari['Air Pollutant'] == pollutant][['Year', 'Air Pollution Average [ug/m3]', 'Premature Deaths']]
-        X = pollutant_data_sassari[['Air Pollution Average [ug/m3]']]
+        pollutant_data_sassari = df_sassari[df_sassari['Air Pollutant'] == pollutant][['Year', 'Air Pollution Population Weighted Average [ug/m3]', 'Premature Deaths']]
+        X = pollutant_data_sassari[['Air Pollution Population Weighted Average [ug/m3]']]
         y = pollutant_data_sassari['Premature Deaths']
         
         model = LinearRegression()
@@ -115,14 +144,13 @@ def analyze_data():
         plt.xlabel(f'{pollutant} Air Pollution Average [ug/m3]')
         plt.ylabel('Premature Deaths')
         plt.legend()
+        plt.tight_layout()
         
-    plt.tight_layout()
-    plt.show()
-    
+    # Plotting Milan info: Air Pollutants and Premature Deaths
+     
 def main():
     data_dir = "data_sets/"
     read_process_data(data_dir)
-    
     analyze_data()
 
 
